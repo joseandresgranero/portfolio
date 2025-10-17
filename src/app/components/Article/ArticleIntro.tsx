146871c78051717589262ca03ee88c70375881eb
Jose Andres Granero <joseandres.granero@gmail.com>
2025-10-17 13:22:15 +0200

    Text line height

--- src/app/components/Article/ArticleIntro.tsx
+++ src/app/components/Article/ArticleIntro.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export const ArticleIntro = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="text-2xl sm:text-3xl leading-10 text-gray-600 my-8">
+    <div className="text-2xl/9 sm:text-3xl sm:leading-10 text-gray-600 my-8">
       {children}
     </div>
   );
--- src/app/components/Article/ArticleTitle.tsx
+++ src/app/components/Article/ArticleTitle.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export const ArticleTitle = ({ children }: { children: React.ReactNode }) => {
   return (
-    <h1 className="text-5xl sm:text-6xl font-heading font-extrabold text-[var(--heading-color)]">
+    <h1 className="text-5xl/14 sm:text-6xl/16 font-heading font-extrabold text-[var(--heading-color)]">
       {children}
     </h1>
   );
