020a940f17c3b829509100b392318d90b97a8137
Jose Andres Granero <joseandres.granero@gmail.com>
2025-10-17 11:51:02 +0200

    Spacing adjustments

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -10,9 +10,9 @@ export default function HeroSection() {
   }, []);
 
   return (
-    <section className="flex flex-col justify-center bg-[var(--hero-bg-color)] lg:h-[calc(var(--hero-height)+var(--header-height))] lg:pb-0 pt-[calc(var(--header-height)+40px)] lg:pt-[var(--header-height)] px-[var(--padding-x-base)] md:px-[var(--padding-x-base-md)] py-10 bg-gradient-to-r from-[var(--primary-color)] to-[var(--primary-color-weakest)] animate-gradient">
+    <section className="flex flex-col justify-center bg-[var(--hero-bg-color)] lg:h-[calc(var(--hero-height)+var(--header-height))] lg:pb-0 pt-[calc(var(--header-height)+60px)] lg:pt-[var(--header-height)] px-[var(--padding-x-base)] md:px-[var(--padding-x-base-md)] py-15 bg-gradient-to-r from-[var(--primary-color)] to-[var(--primary-color-weakest)] animate-gradient">
       <motion.h1
-        className="text-[2.2rem] leading-[1.4] mb-4 text-[var(--hero-text-color)] max-w-[var(--max-width-text-sm)]"
+        className="text-[1.9rem] sm:text-[2.2rem] leading-[1.4] mb-4 text-[var(--hero-text-color)] max-w-[var(--max-width-text-sm)]"
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6, delay: 0.1 }}
